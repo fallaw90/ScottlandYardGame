892aa43e8984f8cc9846c8a1bdb7f5840c1586c6
fallaw90 <37890292+fallaw90@users.noreply.github.com>
2018-03-31 18:54:53 -0500

    Update player.php

--- player.php
+++ player.php
@@ -1,14 +1,58 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Eric
- * Date: 3/27/2018
- * Time: 3:00 PM
- */
-class Player extends Abstract_Character{
+    /**
+     * Created by PhpStorm.
+     * User: Eric
+     * Date: 3/27/2018
+     * Time: 3:00 PM
+     */
 
-    public function __construct(Game $g, $name){
-        $this ->_name = $name;
-        echo "Player {$this->_name} Created:<br>";
+include'cards.php';
+class Player{
+    /**
+    * @var String name of the detective type.
+    */ 
+    public $name;
+    /**
+    * @var int number of ticket taxi.
+    */
+    public $taxi;
+    /**
+	* @var int number of ticket bus.
+	*/
+    public $bus;
+    /**
+	* @var int number of ticket train or underground.
+	*/
+    public $train;
+    /**
+	* @var int the current position of the detective.
+	*/
+    private $position = new cards();
+    
+    /**
+	* @param name, number of tickets taxi, number of tickets bus and number of tickets train of the detective.
+	*/
+    public function __construct(Game $name, $taxi, $bus, $train){
+        $this->name = $name;
+        $this->taxi = $taxi;
+        $this->bus = $bus;
+        $this->train = $train;
+        $this->position = $card.getCard();
     }
+    
+    /**
+	* @param the new position of the detective.
+	*/
+    public function setPosition($newPosition){
+       $this->position = $newPosition;
+    }
+    
+    /**
+	* @return the current position of the detective.
+	*/
+    public function getPosition(){
+        return $this->position;
+    }
+    
+   
 }
