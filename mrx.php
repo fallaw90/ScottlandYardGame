9a8482f7fe0a8b6a46768cf6c899d377eeb44d3b
fallaw90 <37890292+fallaw90@users.noreply.github.com>
2018-03-31 21:03:04 -0500

    Update mrx.php

--- mrx.php
+++ mrx.php
@@ -7,16 +7,91 @@
  */
 // mrx is child class of Abstract_Character, anything both type of characters do
 // should go in there.
-class mrx extends Abstract_Character {
+class Mrx{
     // seperate move function for Mr. X?
-    public function __construct(Game $g, $name){
-        $this ->_name = $name;
+    /**
+    * @var String name Mr. X.
+    */ 
+    public $name;
+    /**
+    * @var int number of taxi tickets.
+    */
+    public $taxi;
+    /**
+    * @var int number of bus tickets.
+    */
+    public $bus;
+    /**
+    * @var int number of underground tickets.
+    */
+    public $train;
+    /**
+    * @var int number of black tickets.
+    */
+    public $blackTicket;
+    /**
+    * @var int number of double.
+    */
+    public $double;
+    /**
+    * @var object card.
+    */
+    private $card = new Card();
+    /**
+    * @var int the current position of Mr X.
+    */
+    private $position;
+    /**
+    * @var associative array for the travel log of Mr X.
+    */
+    private $travelLog = array();
+    
+    
+    /**
+    * @param name, number of taxi tickets, number of bus tickets, number of train tickets and number of black tickets of Mr. X
+    */
+    public function __construct($taxi, $bus, $train, $blackTicket){
+        $this->name = 'Mr X';
+        $this->taxi = $taxi;
+        $this->bus = $bus;
+        $this->blackTicket = $blackTicket;
+        $this->double = 2;
+        $this->position = $card.getCard();
         echo "Mr X {$this->_name} Created:<br>";
     }
-    public function double_move(){
-        echo "Go go gadget shoes!";
-        //mrx moves twice in a row...
+    /**
+    * @param the new position of the detective.
+    */
+    public function move($newPosition){
+        $this->position = $newPosition;
+        if(isset($_POST['taxi'])){
+            $this->taxi --;
+            $this->travelLog[$newPosition] = ['taxi'];
+        }
+        if(isset($_POST['bus'])){
+            $this->bus --;
+            $this->travelLog[$newPosition] = ['bus'];
+        }
+        if(isset($_POST['train'])){
+            $this->bus --;
+            $this->travelLog[$newPosition] = ['train'];
+        }
+        if(isset($_POST['boat'])){
+            $this->blackTicket --;
+            $this->travelLog[$newPosition] = ['blackTicket'];
+        }
+        if(isset($_POST['blackTicket'])){
+            $this->blackTicket --;
+            $this->travelLog[$newPosition] = ['blackTicket'];
+        }
     }
+    
+    public function doubleMove($position1, $position2){
+        move($position1);
+        move($position2);
+        $this->double --;
+    }
+    
     public function location_type(){
         echo "I took a (movement type) here";
         // print to travel log on screen
