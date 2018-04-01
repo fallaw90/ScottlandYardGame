03c50c971908dec6d9d6d597a2e02869049549b4
fallaw90 <37890292+fallaw90@users.noreply.github.com>
2018-04-01 00:40:54 -0500

    Update player.php

--- player.php
+++ player.php
@@ -6,8 +6,18 @@
      * Time: 3:00 PM
      */ 
 
-
+/**
+* Class Player
+* Allow to create a detective. 
+*/    
 class Player extends AbstractData{  
+    
+    /**
+    * @param number of taxi tickets, bus tickets, train tickets and black tickets for the detective
+    */
+    public function __construct($name, $taxi, $bus, $train){
+        parent::__construct($name, $taxi, $bus, $train);
+    }
     
     /**
     * @param the new position of the detective.
