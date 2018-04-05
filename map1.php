b955577c3bd167a58580e9956967a573a9c79fc2
fallaw90 <37890292+fallaw90@users.noreply.github.com>
2018-04-05 11:53:29 -0700

    Add files via upload

---
+++ map1.php
@@ -0,0 +1,17 @@
+<?php
+	if (isset($_POST['station'])) {
+		echo $_POST['station'];
+	}
+
+	echo "<br>";
+
+	if (isset($_POST['lines'])) {
+		echo $_POST['lines'];
+	}
+	
+	echo "<br>";
+
+	if (isset($_POST['nodes'])) {
+		echo $_POST['nodes'];
+	} 
+?>
